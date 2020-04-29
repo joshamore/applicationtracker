bbe43430a93bdef8f9d51b07a371f4a6ea4a4dc4
Josh Amore <joshamore31@gmail.com>
2020-04-30 08:06:45 +1000

    added dashboard route and empty component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/routes/Home";
+import { Dashboard } from "./components/routes/Dashboard";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Route path="/" component={Home} />
+			<Route exact path="/" component={Home} />
+			<Route exact path="/Dashboard" component={Dashboard} />
 		</BrowserRouter>
 	);
 }
---
+++ src/components/routes/Dashboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Dashboard extends React.Component {
+	render() {
+		return <h1>Welcome to the Dashboard</h1>;
+	}
+}
+
+export default Dashboard;
