922f0543750436711d64b7eb14c2fc368005c965
Josh Amore <joshamore31@gmail.com>
2020-04-30 08:12:03 +1000

    Added add route and empty component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./components/routes/Home";
 import { Dashboard } from "./components/routes/Dashboard";
+import { Add } from "./components/routes/Add";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Route exact path="/" component={Home} />
 			<Route exact path="/Dashboard" component={Dashboard} />
+			<Route exact path="/Add" component={Add} />
 		</BrowserRouter>
 	);
 }
---
+++ src/components/routes/Add.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Add extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Add Job Application</h1>
+				<p>This is where you will add your job applications for tracking.</p>
+			</div>
+		);
+	}
+}
+
+export default Add;
