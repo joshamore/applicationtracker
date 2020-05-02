ba716a98f248a51c49cac9f8e46026d6d6d1dee8
Josh Amore <joshamore31@gmail.com>
2020-05-02 18:01:12 +1000

    job application cards are now rendering programatically instead of manually

--- src/components/routes/Dashboard.js
+++ src/components/routes/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Container from "@material-ui/core/Container";
+import Container from "@material-ui/core/Container";
 import { Grid } from "@material-ui/core";
 import Appbar from "../Appbar";
 import JobCard from "../JobCard";
@@ -7,27 +7,31 @@ import JobCard from "../JobCard";
 export class Dashboard extends React.Component {
 	state = {
 		gettingJobs: true,
-		jobsApplications: [],
+		jobApplications: [],
 	};
 
 	componentDidMount() {
 		// Note: faking an API call with settimeout
 		setTimeout(() => {
 			this.setState({
-				jobsApplications: [
+				jobApplications: [
 					{
+						id: 1,
 						jobTitle: "JS Engineer",
 						company: "ABC Startup",
 					},
 					{
+						id: 2,
 						jobTitle: "FrontEnd Engineer",
 						company: "ABC Corp.",
 					},
 					{
+						id: 33,
 						jobTitle: "Rails Dev",
 						company: "XYZ Startup",
 					},
 					{
+						id: 78,
 						jobTitle: "COBAL Engineer",
 						company: "1944 Company",
 					},
@@ -41,37 +45,23 @@ export class Dashboard extends React.Component {
 		return (
 			<div>
 				<Appbar />
-				<h1>Welcome to the Dashboard</h1>
-				{this.state.gettingJobs ? (
-					<p>Loading....</p>
-				) : (
-					<Grid container>
-						<Grid item xs={12} sm={6} md={3}>
-							<JobCard
-								jobTitle={this.state.jobsApplications[0].jobTitle}
-								company={this.state.jobsApplications[0].company}
-							/>
+				<Container maxWidth={false}>
+					<h1>Welcome to the Dashboard</h1>
+					{this.state.gettingJobs ? (
+						<p>Loading....</p>
+					) : (
+						<Grid container>
+							{this.state.jobApplications.map((application) => (
+								<Grid item xs={12} sm={6} md={3}>
+									<JobCard
+										jobTitle={application.jobTitle}
+										company={application.company}
+									/>
+								</Grid>
+							))}
 						</Grid>
-						<Grid item xs={12} sm={6} md={3}>
-							<JobCard
-								jobTitle={this.state.jobsApplications[1].jobTitle}
-								company={this.state.jobsApplications[1].company}
-							/>
-						</Grid>
-						<Grid item xs={12} sm={6} md={3}>
-							<JobCard
-								jobTitle={this.state.jobsApplications[2].jobTitle}
-								company={this.state.jobsApplications[2].company}
-							/>
-						</Grid>
-						<Grid item xs={12} sm={6} md={3}>
-							<JobCard
-								jobTitle={this.state.jobsApplications[3].jobTitle}
-								company={this.state.jobsApplications[3].company}
-							/>
-						</Grid>
-					</Grid>
-				)}
+					)}
+				</Container>
 			</div>
 		);
 	}
