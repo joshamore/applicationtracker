c89e464ad963493ece239a60d75be8a5f201f3ae
Josh Amore <joshamore31@gmail.com>
2020-05-03 17:49:02 +1000

    added spinner to form while pending response and faked api call functionality with setTimout

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
+import Spinner from "./Spinner";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -22,51 +23,70 @@ export default function AddForm({ addApplication }) {
 	const [jobCompany, setjobCompany] = useState("");
 	const [jobLink, setjobLink] = useState("");
 
+	// Stores spinner state
+	const [formSubmitted, setFormSubmitted] = useState(false);
+
 	// Hook used for redirect
 	let history = useHistory();
 
 	return (
 		<div>
 			<Container maxWidth="md">
-				<form className={classes.root} noValidate autoComplete="off">
-					<TextField
-						id="jobTitle"
-						label="Job Title"
-						value={jobTitle}
-						onChange={(e) => setjobTitle(e.target.value)}
-					/>
-					<TextField
-						id="jobCompany"
-						label="Company Name"
-						value={jobCompany}
-						onChange={(e) => setjobCompany(e.target.value)}
-					/>
-					<TextField
-						id="jobLink"
-						label="Link to Ad"
-						value={jobLink}
-						onChange={(e) => setjobLink(e.target.value)}
-					/>
-					<br />
-					<Button
-						id="addApplication"
-						variant="contained"
-						color="primary"
-						onClick={(e) => {
-							// Passing job application data to parent component
-							addApplication({
-								jobTitle: jobTitle,
-								jobCompany: jobCompany,
-								jobLink: jobLink,
-							});
+				{!formSubmitted ? (
+					<form className={classes.root} noValidate autoComplete="off">
+						<TextField
+							id="jobTitle"
+							label="Job Title"
+							value={jobTitle}
+							onChange={(e) => setjobTitle(e.target.value)}
+						/>
+						<TextField
+							id="jobCompany"
+							label="Company Name"
+							value={jobCompany}
+							onChange={(e) => setjobCompany(e.target.value)}
+						/>
+						<TextField
+							id="jobLink"
+							label="Link to Ad"
+							value={jobLink}
+							onChange={(e) => setjobLink(e.target.value)}
+						/>
+						<br />
+						<Button
+							id="addApplication"
+							variant="contained"
+							color="primary"
+							onClick={(e) => {
+								setFormSubmitted(true);
 
-							// Redirecting to dashboard
-							history.push("/dashboard");
-						}}
-					>
-						Add Application
-					</Button>
-				</form>
+								// Passing job application data to parent component
+								addApplication({
+									jobTitle: jobTitle,
+									jobCompany: jobCompany,
+									jobLink: jobLink,
+								})
+									.then((res) => {
+										if (res) {
+											// Redirecting to dashboard
+											history.push("/dashboard");
+										} else {
+											// Rerendering form
+											setFormSubmitted(false);
+										}
+									})
+									.catch((err) => {
+										console.log(err);
+										setFormSubmitted(false);
+									});
+							}}
+						>
+							Add Application
+						</Button>
+					</form>
+				) : (
+					<Spinner />
+				)}
 			</Container>
 		</div>
 	);
--- src/components/routes/Add.js
+++ src/components/routes/Add.js
@@ -4,13 +4,21 @@ import Appbar from "../Appbar";
 import AddForm from "../AddForm";
 
 export class Add extends React.Component {
-	// Sends application data to server
+	// Sends application data to server. Returns a promise boolean with true if successful or false if failed.
 	// TODO: create backend route
 	addApplication = (jobApplication) => {
 		const { jobTitle, jobCompany, jobLink } = jobApplication;
 		console.log(
 			`Applied for ${jobTitle} position with ${jobCompany}. Link to ad: ${jobLink}`
 		);
+
+		// Promise with timout used to mimick API call
+		// TODO: remove after backend created.
+		return new Promise((res, rej) => {
+			setTimeout(() => {
+				res(true);
+			}, 1000);
+		});
 	};
 
 	render() {
