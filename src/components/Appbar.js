a991c043c4644b8159f99cf309d73e75c96b5e81
Josh Amore <joshamore31@gmail.com>
2020-05-05 18:44:54 +1000

    added menu to navbar (routes to add and dashboard)

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -3,18 +3,47 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AppsIcon from "@material-ui/icons/Apps";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
 	},
 	title: {
 		flexGrow: 1,
 	},
 }));
 
-export default function ButtonAppBar() {
+export default function MenuAppBar() {
 	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const history = useHistory();
+	const open = Boolean(anchorEl);
+
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	// Route changes
+	const toAdd = () => {
+		handleClose();
+		history.push("/add");
+	};
+	const toDashboard = () => {
+		handleClose();
+		history.push("/dashboard");
+	};
 
 	return (
 		<div className={classes.root}>
@@ -23,6 +52,35 @@ export default function ButtonAppBar() {
 					<Typography variant="h6" className={classes.title}>
 						Application Tracker
 					</Typography>
+					<div>
+						<IconButton
+							aria-label="Menu to navigate"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleMenu}
+							color="inherit"
+						>
+							<AppsIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorEl}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={open}
+							onClose={handleClose}
+						>
+							<MenuItem onClick={toAdd}>Add Application</MenuItem>
+							<MenuItem onClick={toDashboard}>All Applications</MenuItem>
+						</Menu>
+					</div>
 				</Toolbar>
 			</AppBar>
 		</div>
