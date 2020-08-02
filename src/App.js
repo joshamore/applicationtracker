a4c01602a75f37d6ec2c9f96599477ef889e6829
Josh Amore <joshamore31@gmail.com>
2020-08-02 16:14:07 +1000

    converted Dashboard componenet to hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/routes/Home";
-import { Dashboard } from "./components/routes/Dashboard";
+import Dashboard from "./components/routes/Dashboard";
 import Add from "./components/routes/Add";
 import Job from "./components/routes/Job";
 
--- src/components/routes/Dashboard.js
+++ src/components/routes/Dashboard.js
@@ -1,151 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import { Grid } from "@material-ui/core";
 import Appbar from "../Appbar";
 import JobCard from "../JobCard";
 import Spinner from "../Spinner";
 
-export class Dashboard extends React.Component {
-	state = {
-		gettingJobs: true,
-		jobApplications: [],
-		errorGettingApplications: false,
-	};
+export default function Dashboard() {
+	// Creating state
+	const [gettingJobs, setgettingJobs] = useState(true);
+	const [jobApplications, setjobApplications] = useState([]);
+	const [errorGettingApplications, seterrorGettingApplications] = useState(
+		false
+	);
 
-	async componentDidMount() {
-		// Store for applications
-		let applications;
-		let formattedApplications = [];
+	useEffect(() => {
+		// Using a function here as useEffect doesn't like async
+		// This should be resolved in a future react update (suspense): https://stackoverflow.com/questions/53332321/react-hook-warnings-for-async-function-in-useeffect-useeffect-function-must-ret
 
-		try {
-			// Getting applications
-			applications = await fetch("http://localhost:5000/api/application/all");
-			applications = await applications.json();
+		// Function to get applications
+		async function fetchData() {
+			// Store for applications
+			let applications;
+			let formattedApplications = [];
 
-			// Formatting applications before adding to state
-			applications.forEach((application) => {
-				formattedApplications.push({
-					id: application.app_id,
-					jobTitle: application.app_title,
-					company: application.app_employer,
-					link: application.app_link,
+			try {
+				// Getting applications
+				applications = await fetch("http://localhost:5000/api/application/all");
+				applications = await applications.json();
+
+				// Formatting applications before adding to state
+				applications.forEach((application) => {
+					formattedApplications.push({
+						id: application.app_id,
+						jobTitle: application.app_title,
+						company: application.app_employer,
+						link: application.app_link,
+					});
 				});
-			});
 
-			// Adding formatted applicationst to state
-			this.setState({
-				jobApplications: formattedApplications,
-				gettingJobs: false,
-			});
-		} catch (err) {
-			console.log(err);
-			this.setState({ gettingJobs: false, errorGettingApplications: true });
+				// Adding formatted applicationst to state
+				setjobApplications(formattedApplications);
+				setgettingJobs(false);
+			} catch (err) {
+				console.log(err);
+				setgettingJobs(false);
+				seterrorGettingApplications(true);
+			}
 		}
 
-		// Note: faking an API call with settimeout
-		// setTimeout(() => {
-		// 	this.setState({
-		// 		jobApplications: [
-		// 			{
-		// 				id: 1,
-		// 				jobTitle: "JS Engineer",
-		// 				company: "ABC Startup",
-		// 				link: "https://example.com",
-		// 			},
-		// 			{
-		// 				id: 2,
-		// 				jobTitle: "FrontEnd Engineer",
-		// 				company: "ABC Corp.",
-		// 				link: "https://example.com",
-		// 			},
-		// 			{
-		// 				id: 33,
-		// 				jobTitle: "Rails Dev",
-		// 				company: "XYZ Startup",
-		// 				link: "https://example.com",
-		// 			},
-		// 			{
-		// 				id: 78,
-		// 				jobTitle: "COBAL Engineer",
-		// 				company: "1944 Company",
-		// 				link: "https://example.com",
-		// 			},
-		// 		],
-		// 		gettingJobs: false,
-		// 	});
-		// }, 3000);
+		// Getting applications
+		fetchData();
+	});
+
+	// Returning render with spinner while awaiting applications
+	if (gettingJobs && !errorGettingApplications) {
+		return (
+			<div>
+				<Appbar />
+				<Container maxWidth={false}>
+					<h1>Job Applications</h1>
+					<Spinner />
+				</Container>
+			</div>
+		);
 	}
-
-	render() {
-		if (this.state.gettingJobs && !this.state.errorGettingApplications) {
-			return (
-				<div>
-					<Appbar />
-					<Container maxWidth={false}>
-						<h1>Job Applications</h1>
-						<Spinner />
-					</Container>
-				</div>
-			);
-		} else if (
-			!this.state.gettingJobs &&
-			!this.state.errorGettingApplications
-		) {
-			return (
-				<div>
-					<Appbar />
-					<Container maxWidth={false}>
-						<h1>Job Applications</h1>
-						<Grid container>
-							{this.state.jobApplications.map((application) => (
-								<Grid item xs={12} sm={6} md={3}>
-									<JobCard
-										jobTitle={application.jobTitle}
-										company={application.company}
-										id={application.id}
-									/>
-								</Grid>
-							))}
-						</Grid>
-					</Container>
-				</div>
-			);
-		} else {
-			return (
-				<div>
-					<Appbar />
-					<Container maxWidth={false}>
-						<h1>Job Applications</h1>
-						<p>Unable to get job applications :(</p>
-					</Container>
-				</div>
-			);
-		}
-
-		// return (
-		// 	<div>
-		// 		<Appbar />
-		// 		<Container maxWidth={false}>
-		// 			<h1>Job Applications</h1>
-		// 			{this.state.gettingJobs ? (
-		// 				<Spinner />
-		// 			) : (
-		// 				<Grid container>
-		// 					{this.state.jobApplications.map((application) => (
-		// 						<Grid item xs={12} sm={6} md={3}>
-		// 							<JobCard
-		// 								jobTitle={application.jobTitle}
-		// 								company={application.company}
-		// 								id={application.id}
-		// 							/>
-		// 						</Grid>
-		// 					))}
-		// 				</Grid>
-		// 			)}
-		// 		</Container>
-		// 	</div>
-		// );
+	// Returning render of applications if they were succesfully received
+	else if (!gettingJobs && !errorGettingApplications) {
+		return (
+			<div>
+				<Appbar />
+				<Container maxWidth={false}>
+					<h1>Job Applications</h1>
+					<Grid container>
+						{jobApplications.map((application) => (
+							<Grid item xs={12} sm={6} md={3}>
+								<JobCard
+									jobTitle={application.jobTitle}
+									company={application.company}
+									id={application.id}
+								/>
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</div>
+		);
+	}
+	// Returnig error message if unable to get applications
+	// TODO: add a nicer error message
+	else {
+		return (
+			<div>
+				<Appbar />
+				<Container maxWidth={false}>
+					<h1>Job Applications</h1>
+					<p>Unable to get job applications :(</p>
+				</Container>
+			</div>
+		);
 	}
 }
-
-export default Dashboard;
