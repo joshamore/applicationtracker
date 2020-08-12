359f175b1eb1d090a0756609476e64b68f08229b
Josh Amore <joshamore31@gmail.com>
2020-08-12 13:55:19 +1000

    Added delete button and logic for application items

---
+++ src/components/DeleteItem.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import LinearLoader from "./LinearLoader";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	buttonBuffer: {
+		marginLeft: 10,
+	},
+});
+
+export default function DeleteItem({
+	itemID,
+	setIsError,
+	setErrorMessage,
+	newItemAdded,
+}) {
+	const classes = useStyles();
+
+	// Setting state
+	const [open, setOpen] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const deleteItem = async () => {
+		// Setting loading state
+		setIsDeleting(true);
+
+		// Delete Confirm var
+		let confirm;
+
+		// Getting token
+		const token = localStorage.getItem("token");
+
+		try {
+			// Attempting to delete application item
+			confirm = await fetch(
+				`http://localhost:5000/api/application/item/?id=${itemID}`,
+				{
+					method: "DELETE",
+					headers: {
+						"auth-token": token,
+					},
+				}
+			);
+			confirm = await confirm.json();
+
+			if (confirm.success === true) {
+				setIsDeleting(false);
+				setOpen(false);
+
+				// Reloading items after delete
+				newItemAdded();
+			} else {
+				// If error, throwing to catch
+				throw Error(confirm.err);
+			}
+		} catch (err) {
+			// Logging error
+			console.log(`ERROR DELETING APPLICATION ITEM: ${itemID} ERROR: ${err}`);
+
+			// Setting error handler
+			setErrorMessage("Unablet to delete item 😥");
+			setIsError(true);
+
+			// Closing delete
+			setIsDeleting(false);
+			setOpen(false);
+		}
+	};
+
+	// Setting linear loader if delete in progress
+	if (isDeleting) {
+		return (
+			<div>
+				<Button
+					className={classes.buttonBuffer}
+					variant="contained"
+					color="secondary"
+					onClick={handleClickOpen}
+				>
+					<DeleteIcon />
+				</Button>
+				<Dialog
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="form-dialog-title"
+				>
+					<LinearLoader />
+					<DialogTitle id="form-dialog-title">
+						Delete Application Item
+					</DialogTitle>
+					<DialogContent>
+						<DialogContentText>
+							Clicking Yes below will remove this job application and all
+							related items. <strong>Are you sure</strong>?
+						</DialogContentText>
+					</DialogContent>
+				</Dialog>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<Button
+					className={classes.buttonBuffer}
+					variant="contained"
+					color="secondary"
+					onClick={handleClickOpen}
+				>
+					<DeleteIcon />
+				</Button>
+				<Dialog
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="form-dialog-title"
+				>
+					<DialogTitle id="form-dialog-title">
+						Delete Application Item
+					</DialogTitle>
+					<DialogContent>
+						<DialogContentText>
+							Clicking Yes below will remove this application item.{" "}
+							<strong>Are you sure</strong>?
+						</DialogContentText>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose} color="primary">
+							No, keep this item
+						</Button>
+						<Button onClick={deleteItem} color="secondary">
+							Yes, delete this item
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</div>
+		);
+	}
+}
